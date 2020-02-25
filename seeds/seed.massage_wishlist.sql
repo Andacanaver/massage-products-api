ee24f6360db82b4ad9e81740f8507d53fe3549c6
Jacob M <48130732+Andacanaver@users.noreply.github.com>
2020-02-25 13:28:42 -0600

    update seed files

---
+++ seeds/seed.massage_wishlist.sql
@@ -0,0 +1,17 @@
+TRUNCATE 
+    massage_wishlist,
+    massage_wishlist_users,
+    massage_wishlist_products
+RESTART IDENTITY CASCADE;
+
+INSERT INTO massage_wishlist (wishlist_name)
+VALUES 
+    ('Wishlist One'), ('Wishlist Two'), ('Wishlist Three'), ('Wishlist One'), ('Wishlist Four');
+
+INSERT INTO massage_wishlist_users (user_id, wishlist_id)
+VALUES
+    (1, 1), (1, 2), (2, 3), (2, 4);
+
+INSERT INTO massage_wishlist_products (product_id, wishlist_id)
+VALUES
+    (1, 1), (2, 1), (3, 1), (7, 1), (1, 2), (2, 2), (7, 3), (4, 3);
